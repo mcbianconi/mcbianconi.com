685db74dbeea31120ca69aac4e844cd99115dfed
Murillo Cesar Bianconi <murillo.bianconi@gmail.com>
2026-01-25 10:42:42 -0300

    flaking

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "My Personal Site";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.hugo
+          ];
+
+          shellHook = ''
+            echo ""
+            echo "Hugo $(hugo version | cut -d' ' -f2)"
+            echo ""
+            echo "Commands:"
+            echo "  dev        → http://localhost:1313"
+            echo "  deploy     → commit + push"
+            echo ""
+
+            dev() {
+              hugo server -D --navigateToChanged
+            }
+
+            deploy() {
+              git add -A
+              git commit -m "$(date '+%Y-%m-%d %H:%M') - atualização"
+              git push
+            }
+          '';
+        };
+      }
+    );
+}
